889df9d93cbeb56f39c60c16675c237c391dcbc2
wooJake <167344799+wooJake@users.noreply.github.com>
2024-04-29 10:22:27 -0700

    Create Base_Conversion.cpp

---
+++ Base_Conversion.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+     
+     //Whole number base 10 to other base.
+     unsigned int decNumber;
+     unsigned int base;
+     
+     cout << "Enter decimal value: ";
+     cin >> decNumber;
+     cout << endl;
+     
+     cout << "Enter base to convert to: ";
+     cin >> base;
+     cout << endl;
+     
+     int x = decNumber;
+     
+     for(int i = 0; x >= 1; ++i){
+          
+          x /= base;
+          cout << x << "::" << decNumber % base << endl;
+          decNumber = x;
+     }
+
+     return 0;
+}
